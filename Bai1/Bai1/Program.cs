dca8b0e297b841a08c9adb531512b9dcde7f1b84
HoangVanQuocNhat <hoangnhat250606@gmail.com>
2026-03-18 23:07:50 +0700

    Bai 1

---
+++ Bai1/Bai1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoang Van Quoc Nhat 2415053122332\n");
+            List<int> ints = new List<int> { 1, 2, 3, 4, 5 };
+
+            foreach (int i in ints)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
