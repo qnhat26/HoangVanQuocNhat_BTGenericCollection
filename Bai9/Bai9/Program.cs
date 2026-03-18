d6c47c08e332252d8c1aa9c37fb61922348e26f5
HoangVanQuocNhat <hoangnhat250606@gmail.com>
2026-03-18 23:14:50 +0700

    Bai9

---
+++ Bai9/Bai9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoang Van Quoc Nhat 2415053122332\n");
+            List<int> list = new List<int>();
+
+            Console.Write("Nhap so luong phan tu: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"arr[{i}] = ");
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[i] == list[j])
+                    {
+                        list.RemoveAt(j);
+                        j--;
+                    }
+                }
+            }
+            Console.WriteLine("\nDanh sach khong lap: ");
+            foreach (var i in list)
+                Console.WriteLine(i);
+        }
+    }
+}
