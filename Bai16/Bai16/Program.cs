0bd655ee2e05cc8130d5503f7112d18cbb521989
HoangVanQuocNhat <hoangnhat250606@gmail.com>
2026-03-18 23:19:13 +0700

    Bai16

---
+++ Bai16/Bai16/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoang Van Quoc Nhat 2415053122332\n");
+            Console.Write("Nhap chuoi :");
+            string s = Console.ReadLine();
+
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+
+            foreach (char c in s)
+                if(dic.ContainsKey(c))
+                    dic[c] ++;
+                else
+                    dic[c] = 1;
+            foreach(var item in dic)
+                Console.WriteLine($"{item.Key}:{item.Value}");
+        }
+    }
+}
