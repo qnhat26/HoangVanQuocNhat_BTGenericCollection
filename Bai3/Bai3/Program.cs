6d2db3ec5a2db1da57cb9f82cb919507aeaace03
HoangVanQuocNhat <hoangnhat250606@gmail.com>
2026-03-18 23:09:19 +0700

    Bai3

---
+++ Bai3/Bai3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoang Van Quoc Nhat 2415053122332\n");
+            List<int> list = new List<int>();
+
+            Console.Write("Nhap so luong phan tu: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"arr[{i}] = ");
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+
+            int max = list[0];
+            foreach (int i in list)
+                if(i > max)
+                    max=i;
+            Console.WriteLine("So lon nhat :"+max);
+        }
+    }
+}
