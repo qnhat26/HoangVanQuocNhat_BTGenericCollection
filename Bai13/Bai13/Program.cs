849fab4dfb113ead1896e0e3eb8b061486ffce80
HoangVanQuocNhat <hoangnhat250606@gmail.com>
2026-03-18 23:19:00 +0700

    Bai13

---
+++ Bai13/Bai13/Program.cs
@@ -0,0 +1,26 @@
+﻿using Bai11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoang Van Quoc Nhat 2415053122332\n");
+            List<Student> students = new List<Student>
+            {
+            new Student { Id = 1, Name = "Thinh" },
+            new Student { Id = 2, Name = "Nhat" },
+            new Student { Id = 3, Name = "Hoang" },
+            new Student { Id = 4, Name = "Tien" }
+            };
+
+            Console.WriteLine("Danh sach co "+students.Count()+" sinh vien");
+        }
+    }
+}
